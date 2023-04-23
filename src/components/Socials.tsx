fc031f6e3cc910016582aac9c2f251550e3873c8
theinsideshine <educacion.ta@gmail.com>
2023-04-23 20:38:26 -0300

    add social links

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,8 +1,70 @@
-import {FC, memo} from 'react';
+import {FC, memo, useEffect ,useState} from 'react';
 
-import {socialLinks} from '../data/data';
+//import {socialLinks} from '../data/data';
+
+import { fetchWithoutToken } from '../helpers/fetch';
+
+import {
+ 
+  Social,
+ 
+} from '../data/dataDef';
+
+import GithubIcon from '../components/Icon/GithubIcon';
+import InstagramIcon from '../components/Icon/InstagramIcon';
+import LinkedInIcon from '../components/Icon/LinkedInIcon';
+import  DribbbleIcon  from '../components/Icon/DribbbleIcon';
+import FacebookIcon from '../components/Icon/FacebookIcon';
+
+
+
+
 
 const Socials: FC = memo(() => {
+
+
+  const [githubLink, setGithubLink]= useState('');
+  const [youtubeLink, setYoutubeLink]= useState('');
+  const [linkedinLink, setLinkedinLink]= useState('');
+  const [igLink, setIgLink]= useState('');
+  const [facebookLink, setFacebookLink]= useState('');
+
+
+  useEffect(() => {
+
+    const ApiCall = async () => {    
+
+      const response = await fetchWithoutToken('section/hero/data',0,'GET');
+   
+      const body = await response.json();                          
+
+     console.log(body);
+    
+      if(response.status === 200) {  
+        
+        setGithubLink(body.githubLink); 
+        setYoutubeLink(body.youtubeLink);
+        setLinkedinLink(body.linkedinLink);   
+        setIgLink(body.igLink);
+        setFacebookLink(body.facebookLink);        
+       
+      } else {
+        console.error('error en fetch');
+         //Inconsistency in the error messages, it is necessary to reform the return of public List<Users> getUsers in the back
+             }    
+    }
+    ApiCall();    
+},[]);
+
+ const socialLinks: Social[] = [
+  {label: 'Github', Icon: GithubIcon, href: githubLink},
+  {label: 'Youtube', Icon: DribbbleIcon, href: youtubeLink},
+  {label: 'LinkedIn', Icon: LinkedInIcon, href: linkedinLink},
+  {label: 'Instagram', Icon: InstagramIcon, href: igLink},
+  {label: 'Facebook', Icon: FacebookIcon, href: facebookLink}
+  
+];
+
   return (
     <>
       {socialLinks.map(({label, Icon, href}) => (
